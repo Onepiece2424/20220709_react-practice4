a71d57368333d419dfafa2e50564a68eb995c976
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-09 21:17:45 +0900

    引数の()の段階で分割代入

--- src/components/ColoredMessage.jsx
+++ src/components/ColoredMessage.jsx
@@ -1,7 +1,6 @@
-export const ColoredMessage = (props) => {
-  //propsを分割代入
-  const { color, children } = props;
+//引数の()の段階で分割代入
 
+export const ColoredMessage = ({ color, children }) => {
   const contentStyle = {
     color, // props.が不要。また、Jsの省略記法の使用
     fontSize: "20px"
