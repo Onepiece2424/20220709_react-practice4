18c66d180e2f6b4eed3325d2b32155302d4a41c7
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-09 20:10:56 +0900

    テキストの色を青色に変更

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ export const App = () => {
   const onClickButton = () => {
     alert();
   };
+
+  const contentStyle = {
+    color: "blue",
+    fontSize: "20px"
+  };
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <p>お元気ですか？</p>
+      <p style={contentStyle}>お元気ですか？</p>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
