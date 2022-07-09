d9aa1ee01287501e44241762f16b30881c99282e
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-09 20:43:28 +0900

    propsにより、色と文字を変更することができるコンポーネントの作成

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,13 @@ import { ColoredMessage } from "./components/ColoredMessage";
 
 export const App = () => {
   const onClickButton = () => {
-    alert();
-  };
-
-  const contentPinkStyle = {
-    color: "pink",
-    fontSize: "20px"
+    alert("今日も良い天気ですね〜〜");
   };
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColoredMessage color="blue" message="お元気ですか？" />
-      <p style={contentPinkStyle}>元気です！</p>
+      <ColoredMessage color="pink" message="元気です！" />
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
