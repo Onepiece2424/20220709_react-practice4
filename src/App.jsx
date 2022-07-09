43b7545ad5ced8fd7499c809e591c35f67f66f7e
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-09 20:28:29 +0900

    ColoredMessageコンポーネントの作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
+import { ColoredMessage } from "./components/ColoredMessage";
+
 export const App = () => {
   const onClickButton = () => {
     alert();
   };
 
-  const contentStyle = {
-    color: "blue",
+  const contentPinkStyle = {
+    color: "pink",
     fontSize: "20px"
   };
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <p style={contentStyle}>お元気ですか？</p>
+      <ColoredMessage />
+      <p style={contentPinkStyle}>元気です！</p>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
